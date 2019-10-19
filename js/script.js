77d6eabd22f1edd55b68730a17dfc65b9507eba9
*allan9595* <bohanzhang@my.unt.edu>
2019-10-19 01:18:23 -0400

    build cards && git push origin v1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// let card = document.querySelectorAll('.card');
+// card.addEventListener('click', () => {
+//   card.classList.toggle('is-flipped');
+// });
+
+// $('img').each((index, element) => {
+//   console.log(element)
+//   $(element).click((e) => {
+//     console.log(e.target)
+//     $(e.target).toggleClass('is-flipped');
+//   })
+// })
