6121fdf5c814cd38ee7b5a6405fbc9f3683408a4
*allan9595* <bohanzhang@my.unt.edu>
2019-10-21 14:14:36 -0400

    stylying sidebar

--- js/script.js
+++ js/script.js
@@ -92,24 +92,24 @@ if($(window).width() >= 768){
         )
     })
 
-    // // When the user scrolls the page, execute myFunction
-    // window.onscroll = () =>  {
-    //     createSticky()
-    // };
+      // When the user scrolls the page, execute myFunction
+      window.onscroll = () =>  {
+        createSticky()
+    };
 
-    // // Get the header
-    // var header = document.getElementById("header");
-    // // Get the offset position of the navbar
-    // var sticky = header.offsetTop -100;
+    // Get the header
+    var header = document.getElementById("header");
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
 
-    // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    // const createSticky = () => {
-    //     if (window.pageYOffset  > sticky) {
-    //         header.classList.add("sticky");
-    //     } else {
-    //         header.classList.remove("sticky");
-    //     }
-    // }
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    const createSticky = () => {
+        if (window.pageYOffset  > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
 }
 
 
