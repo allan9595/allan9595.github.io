da3d5bd57e3b0ed02879509ca14355c8538c897b
unknown <bohanzhang@my.unt.edu>
2019-10-22 13:10:51 -0400

    update menu icon size

--- js/script.js
+++ js/script.js
@@ -83,8 +83,8 @@ if($(window).width() >= 768){
             <svg 
                 class="menu-close-sidebar"
                 xmlns="http://www.w3.org/2000/svg" 
-                width="24" 
-                height="24" 
+                width="35" 
+                height="35"  
                 viewBox="0 0 24 24">
                 <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
             </svg>
